1afadbfadd78db127aa64505178e835f41f859aa
pritamrangari27 <pritamrangari125@gmail.com>
2026-02-04 19:40:22 +0530

    Fix galleryData to match available photos

--- client/src/data/galleryData.ts
+++ client/src/data/galleryData.ts
@@ -15,45 +15,21 @@ export const galleryImages: GalleryImage[] = [
   },
   {
     id: 2,
-    src: '/images/eventPhotos/event2.jpeg',
-    title: 'Green Club IOIT',
-    team: ['Chinmay Nakwa']
-  },
-  {
-    id: 3,
     src: '/images/eventPhotos/event3.jpeg',
     title: 'Website',
     team: ['Swaroop Patil', 'Ashish Kharde']
   },
   {
-    id: 4,
-    src: '/images/eventPhotos/event4.jpeg',
-    title: 'AI Graveyard',
-    team: ['Swaroop Patil']
-  },
-  {
-    id: 5,
-    src: '/images/eventPhotos/event5.jpeg',
+    id: 3,
+    src: '/images/eventPhotos/event5.jpg',
     title: 'Jeevan',
     team: ['Swaroop Patil', 'Bhumi Shinde']
   },
   {
-    id: 6,
-    src: '/images/eventPhotos/event6.jpeg',
+    id: 4,
+    src: '/images/eventPhotos/event6.jpg',
     title: 'Devrai',
     team: ['Sarvesh Kolte']
-  },
-  {
-    id: 7,
-    src: '/images/eventPhotos/event7.jpeg',
-    title: 'Sahabhag',
-    team: ['Swaroop Patil']
-  },
-  {
-    id: 8,
-    src: '/images/eventPhotos/event8.jpeg',
-    title: 'MLSA Club IOIT',
-    team: ['Pritam Rangari', 'Amol Waghmare']
   },
 ];
 
